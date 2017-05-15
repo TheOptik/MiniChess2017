7fd7f50c0ec0db6604987d3a2241e2f2a020e4f1
Hümmer, Simon <simon.huemmer@huemmer-satz.de>
2017-05-15 13:32:27 +0200

    further implemented unit tests

--- test/de/doe/BoardTest.java
+++ test/de/doe/BoardTest.java
@@ -1,8 +1,5 @@
 package de.doe;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 import de.doe.figures.Figure;
@@ -12,6 +9,10 @@ public class BoardTest {
 	@Test
 	public void boardDefaultLayoutTest() throws Exception {
 		Board board = new Board();
+		
+		// Test board parameter
+		assertEquals(board.activePlayer, Player.WHITE);
+		assertEquals(board.turn, 1);
 		
 		// Test Black Pieces
 		assertEquals(board.getField(0, 0).figure, Figure.BLACK_KING);
@@ -33,7 +34,7 @@ public class BoardTest {
 		assertEquals(board.getField(3, 5).figure, Figure.WHITE_KNIGHT);
 		assertEquals(board.getField(4, 5).figure, Figure.WHITE_ROOK);
 		
-		// Test all null pieces
+		// Test null Pieces
 		for (int x = 0; x < 5; x++) {
 			for (int y = 2; y < 4; y++) {
 				assertEquals(board.getField(x, y).figure, null);
@@ -42,10 +43,54 @@ public class BoardTest {
 		
 	}
 	
-	@Ignore
 	@Test
 	public void readBoardLayoutTest() throws Exception {
 		
+		Board board = new Board(getMultilineString("20 B", "ppKP.", "QNnRp", ".....", ".....", ".....", "....."));
+		
+		// Test board parameter
+		assertEquals(board.turn, 20);
+		assertEquals(board.activePlayer, Player.BLACK);
+		
+		// Test Black Pieces
+		assertEquals(board.getField(0, 0), Figure.BLACK_PAWN);
+		assertEquals(board.getField(1, 0), Figure.BLACK_PAWN);
+		assertEquals(board.getField(2, 1), Figure.BLACK_KNIGHT);
+		
+		// Test White Pieces
+		assertEquals(board.getField(2, 0), Figure.WHITE_KING);
+		assertEquals(board.getField(3, 0), Figure.WHITE_PAWN);
+		assertEquals(board.getField(0, 1), Figure.WHITE_QUEEN);
+		assertEquals(board.getField(1, 1), Figure.WHITE_KNIGHT);
+		assertEquals(board.getField(3, 1), Figure.WHITE_ROOK);
+		
+		// Test null Pieces
+		
+		assertEquals(board.getField(4, 0).figure, null);
+		for (int x = 0; x < 5; x++) {
+			for (int y = 2; y < 6; y++) {
+				assertEquals(board.getField(x, y).figure, null);
+			}
+		}
+	}
+	
+	private String getMultilineString(String... strings) {
+		StringBuilder resultBuilder = new StringBuilder();
+		for (String s : strings) {
+			resultBuilder.append(s);
+		}
+		return resultBuilder.toString();
+	}
+	
+	/**
+	 * this method is to turn around the arguments for the JUnit-Assertion
+	 * because i consistently do it wrong (i like it better this way anyways).
+	 * 
+	 * @param actual
+	 * @param expected
+	 */
+	private void assertEquals(Object actual, Object expected) {
+		org.junit.Assert.assertEquals(expected, actual);
 	}
 	
 }
