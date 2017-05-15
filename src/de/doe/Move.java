ce9b3a3294ff54c4f0e3b16c457524e92938c0b2
Lucius <pascal.mueller@student.fhws.de>
2017-05-15 15:23:35 +0200

    update Class Move

--- src/de/doe/Move.java
+++ src/de/doe/Move.java
@@ -6,6 +6,12 @@ public class Move {
   Field to;
   
   public Move(String move){
+    move = move.toLowerCase();
     
+  }
+  
+  @Override
+  public String toString() {
+    return "From: " + from + " To: " + to;
   }
 }
