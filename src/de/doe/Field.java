9d03c3d69e93391f51265e784898fb97cc725067
Lucius <pascal.mueller@student.fhws.de>
2017-05-15 14:18:24 +0200

    added getter und setter to Field. Added default into the switch-case

--- src/de/doe/Field.java
+++ src/de/doe/Field.java
@@ -3,20 +3,29 @@ package de.doe;
 import de.doe.figures.Figure;
 
 public class Field {
-	
-	protected Figure figure;
-	
-	public Field(Figure figure) {
-		super();
-		this.figure = figure;
-	}
-	
-	@Override
-	public String toString() {
-		if (figure != null) {
-			return "<" + figure.toString() + ">";
-		} else {
-			return "<null>";
-		}
-	}
+
+  protected Figure figure;
+
+  public Field(Figure figure) {
+    super();
+    this.figure = figure;
+  }
+
+  @Override
+  public String toString() {
+    if (figure != null) {
+      return "<" + figure.toString() + ">";
+    } else {
+      return "<null>";
+    }
+  }
+
+  public Figure getFigure() {
+    return figure;
+  }
+
+  public void setFigure(Figure figure) {
+    this.figure = figure;
+  }
+
 }
