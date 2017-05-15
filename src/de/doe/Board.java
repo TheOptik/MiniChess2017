a3797e1290137a7b3ae4f6e7a9b37665e44d86b0
Hümmer, Simon <simon.huemmer@huemmer-satz.de>
2017-05-15 11:32:24 +0200

    created BoardStub

--- src/de/doe/Board.java
+++ src/de/doe/Board.java
@@ -1,5 +1,30 @@
 package de.doe;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class Board {
+	
+	public Board() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Board(String board) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Board(BufferedReader reader) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	public void print(PrintWriter writer) {
+		// TODO Auto-generated method stub
+	}
 	
 }
