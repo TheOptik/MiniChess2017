f1a86294bdf593d4dcc6b2ee18274f29dc69a0b8
Hümmer, Simon <simon.huemmer@huemmer-satz.de>
2017-05-15 12:16:10 +0200

    wrote first test and added some project management stuff to the pom

--- test/de/doe/BoardTest.java
+++ test/de/doe/BoardTest.java
@@ -1,12 +1,45 @@
 package de.doe;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import de.doe.figures.Figure;
 
 public class BoardTest {
 	
 	@Test
-	public void boardConstructorTest() throws Exception {
+	public void boardDefaultLayoutTest() throws Exception {
 		Board board = new Board();
+		
+		// Test Black Pieces
+		assertEquals(board.getField(0, 0), Figure.BLACK_KING);
+		assertEquals(board.getField(1, 0), Figure.BLACK_QUEEN);
+		assertEquals(board.getField(2, 0), Figure.BLACK_BISHOP);
+		assertEquals(board.getField(3, 0), Figure.BLACK_KNIGHT);
+		assertEquals(board.getField(4, 0), Figure.BLACK_ROOK);
+		for (int i = 0; i < 5; i++) {
+			assertEquals(board.getField(i, 1), Figure.BLACK_PAWN);
+		}
+		
+		// Test WhitePieces
+		assertEquals(board.getField(0, 0), Figure.WHITE_KING);
+		assertEquals(board.getField(1, 0), Figure.WHITE_QUEEN);
+		assertEquals(board.getField(2, 0), Figure.WHITE_BISHOP);
+		assertEquals(board.getField(3, 0), Figure.WHITE_KNIGHT);
+		assertEquals(board.getField(4, 0), Figure.WHITE_ROOK);
+		for (int i = 0; i < 5; i++) {
+			assertEquals(board.getField(i, 1), Figure.WHITE_PAWN);
+		}
+	}
+	
+	
+	@Test
+	public void readBoardLayoutTest() throws Exception {
+		
+		
+		
+		
 	}
 	
 }
