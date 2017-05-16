170cb75114653adfde1a737fe18d2043bb6b7b4e
Lucius <pascal.mueller@student.fhws.de>
2017-05-16 11:18:54 +0200

    implementated MoveMode

--- src/de/doe/MoveMode.java
+++ src/de/doe/MoveMode.java
@@ -1,5 +1,24 @@
 package de.doe;
 
 public class MoveMode {
+ 
+  public static final int CAPUTRE_MODE_NO = 0;
+  public static final int CAPUTRE_MODE_CAN = 1;
+  public static final int CAPUTRE_MODE_MUST = 2;
+  
+  public int columnDirection;
+  public int rowDirection;
+  public boolean oneStep;
+  public int catureMode;
+  
+  public MoveMode(int columnDirection, int rowDirection, boolean oneStep, int catureMode) {
+    super();
+    this.columnDirection = columnDirection;
+    this.rowDirection = rowDirection;
+    this.oneStep = oneStep;
+    this.catureMode = catureMode;
+  }
+ 
 
 }
+
